01c653bb40839e77460311c4d7a3e8a6f1c45484
JJJJiwon <jwjw0752@gmail.com>
2023-09-15 15:28:39 +0900

    refectory

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,14 +8,11 @@ def do_fizzbuzz(num:int):
     나머지는 숫자 그대로를 출력합니다.
     """
     for i in range(1,num+1):
-        if i%15==0:
-            print('fizzbuzz')
-        elif i%3==0:
-            print('fizz')
-        elif i%5==0:
-            print('buzz')
+        if i%3==0 or I%5==0:
+            print('fizz'*(i%3==0) + 'buzz'%(i%5==0))
         else:
             print(i)
 
 if __name__=='__main__':
     do_fizzbuzz(16)
+
